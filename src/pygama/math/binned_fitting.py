39e8de17afb25e2439740b38a1a49fa4bef64c07
Valerio D'Andrea <dandrea@legend-login1.lngs.infn.it>
2025-03-29 21:56:54 +0100

    pargen changes

--- src/pygama/math/binned_fitting.py
+++ src/pygama/math/binned_fitting.py
@@ -442,3 +442,12 @@ def taylor_mode_max(
     cov_jact = np.matmul(cov, jac.transpose())
     cov = np.matmul(jac, cov_jact)
     return (mode, maximum), cov
+
+
+def gauss_amp_uniform(x, a, mu, sigma, a_bkg):
+    if sigma == 0:
+        invs = np.nan
+    else:
+        invs = 1.0 / sigma
+    z = (x - mu) * invs
+    return a * np.exp(-0.5 * z**2) + a_bkg
